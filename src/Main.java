7aef7ab0fd514bfc97642ac2e61bafd5de85a1b7
Zydson123 <mlgplaygaming@gmail.com>
2022-12-10 21:57:27 +0100

    pretty sure everything works

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,20 @@ import java.util.Scanner;
 import java.util.SortedMap;
 
 public class Main {
+    public static void print_hand(ArrayList<Card> Cards){
+        for (int i = 0; i < Cards.size(); i++)
+        {
+            System.out.println(i+1 + ": " + Cards.get(i));
+        }
+    }
     public static void main(String[] args) {
         int cardsForOpponent=0;
+        int blockForOpponent=0;
         Player player1 = new Player("Kuba");
         Player player2 = new Player("Grzesiu");
         Card card = new Card("pies", "pies");
         card.fillDeck(card.getDeck());
         ArrayList<Card> Deck = card.getDeck();
-        System.out.println(Deck);
         Player turn = player1;
         player1.giveCards(Deck, player1);
         player2.giveCards(Deck, player2);
@@ -18,7 +24,7 @@ public class Main {
         boolean validCard=false;
         Card CardInCenter = Deck.get(cardId);
         while(!validCard){
-            if(CardInCenter.getType()!="Block" && CardInCenter.getType()!="+4" && CardInCenter.getType()!="Ch" && CardInCenter.getType()!="Switch"){
+            if(CardInCenter.getType()!="Block" && CardInCenter.getType()!="+4" && CardInCenter.getType()!="+2" && CardInCenter.getType()!="Ch" && CardInCenter.getType()!="Switch"){
                 CardInCenter = Deck.get(cardId);
                 validCard=true;
             }
@@ -26,26 +32,46 @@ public class Main {
                 cardId = (int)(Math.random() * Deck.size());
             }
         }
-        player1.getCards().add(new Card("+4","None"));
-        player2.getCards().add(new Card("+4","None"));
-        player1.getCards().add(new Card("Ch","None"));
-        player2.getCards().add(new Card("Ch","None"));
-        player1.getCards().add(new Card("+2","Red"));
-        player2.getCards().add(new Card("+2","Red"));
+        CardInCenter = Deck.get(0);
         while (true) {
-            System.out.println("The card in the center is: " + CardInCenter);
             if(player1.getCards().size()==0){
                 System.out.println(player1.getName() + " Wins!");
+                break;
             }
             else if(player2.getCards().size()==0){
                 System.out.println(player2.getName() + " Wins!");
+                break;
             }
             else if (turn == player1) {
-                if(cardsForOpponent!=0){
+                System.out.println("The card in the center is: " + CardInCenter);
+                if(blockForOpponent!=0){
+                    System.out.println("It's the turn of: " + player1.getName() + " you have been blocked for: " + blockForOpponent + " turns");
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Choose your card (type in 2137  if you dont have or dont want to use your block type in: 2137)");
+                    print_hand(player1.getCards());
+                    int whichCard = scan.nextInt();
+                    whichCard--;
+                    if(whichCard==2136) {
+                        turn = player2;
+                        blockForOpponent--;
+                    }
+                    else {
+                            Card chosenCard = player1.getCards().get(whichCard);
+                            if (chosenCard.getType() == "Block") {
+                                CardInCenter = chosenCard;
+                                blockForOpponent++;
+                                turn = player2;
+                            }
+                            else if (player2.getCards().get(whichCard).getType() != "Block") {
+                                System.out.println("This is not a card you can use!");
+                            }
+                        }
+                }
+                else if(cardsForOpponent!=0){
                     System.out.println("It's the turn of: " + player1.getName() + " he has " + cardsForOpponent + " cards to draw, if he doesnt use a + card he will draw the amount");
                     Scanner scan = new Scanner(System.in);
                     System.out.println("Choose your card (type in 2137 if you dont have/dont want to use your + cards)");
-                    System.out.println(player1.getCards());
+                    print_hand(player1.getCards());
                     int whichCard = scan.nextInt();
                     whichCard--;
                     if(whichCard==2136){
@@ -55,11 +81,48 @@ public class Main {
                             Deck.remove(Deck.get(cardId2));
                             cardsForOpponent--;
                         }
-                        turn = player2;
+                        turn=player2;
+                    }
+                    else{
+                        Card chosenCard = player1.getCards().get(whichCard);
+                            if(chosenCard.getType()=="+2"){
+                                cardsForOpponent+=2;
+                                CardInCenter = chosenCard;
+                                player1.getCards().remove(chosenCard);
+                                turn = player2;
+                            }
+                            else if(chosenCard.getType()=="+4"){
+                                Scanner color = new Scanner(System.in);
+                                System.out.println("Choose the color (Red,Green,Blue,Yellow");
+                                String CardColor = color.nextLine();
+                                CardInCenter = new Card("0",CardColor);
+                                cardsForOpponent+=4;
+                                player1.getCards().remove(chosenCard);
+                                turn = player2;
+                            }
+                            else{
+                                System.out.println("You can't use this card to counter the + card!");
+                            }
+                    }
+
+                }
+                else if(cardsForOpponent!=0 && blockForOpponent!=0){
+                    System.out.println("It's the turn of: " + player1.getName() + " he has " + cardsForOpponent + " cards to draw, if he doesnt use a + card he will draw the amount");
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Choose your card (type in 2137 if you dont have/dont want to use your + cards)");
+                    print_hand(player1.getCards());
+                    int whichCard = scan.nextInt();
+                    whichCard--;
+                    if(whichCard==2136){
+                        int cardId2 = (int)(Math.random() * Deck.size());
+                        player1.getCards().add(Deck.get(cardId2));
+                            Deck.remove(Deck.get(cardId2));
+                            turn=player2;
                     }
                     else{
                         Card chosenCard = player1.getCards().get(whichCard);
                         if(chosenCard.getType()=="+2"){
+                            CardInCenter = chosenCard;
                             cardsForOpponent+=2;
                             turn = player2;
                         }
@@ -77,20 +140,19 @@ public class Main {
                 else {
                     System.out.println("It's the turn of: " + player1.getName());
                     System.out.println("Your hand: ");
-                    System.out.println(player1.getCards());
+                    print_hand(player1.getCards());
                     Scanner scan = new Scanner(System.in);
                     System.out.println("Choose your card (type in 2137 to skip your turn)");
                     int whichCard = scan.nextInt();
                     whichCard--;
                     if (whichCard == 2136) {
-                        player1.getCards().add(Deck.get(0));
-                        Deck.remove(Deck.get(0));
+                        int cardId2 = (int)(Math.random() * Deck.size());
+                        player1.getCards().add(Deck.get(cardId2));
+                        Deck.remove(Deck.get(cardId2));
                         turn = player2;
                     }
                     else {
                         Card chosenCard = player1.getCards().get(whichCard);
-                        System.out.println(chosenCard.getType());
-                        System.out.println(chosenCard.getColor());
                         if (chosenCard.getType() == CardInCenter.getType() || chosenCard.getColor() == CardInCenter.getColor() || chosenCard.getColor() == "None") {
                             if (chosenCard.getColor() == "None") {
                                 if (chosenCard.getType() == "Ch") {
@@ -106,10 +168,26 @@ public class Main {
                                     System.out.println("Choose the color (Red,Green,Blue,Yellow");
                                     String CardColor = color.nextLine();
                                     CardInCenter = new Card("0", CardColor);
-                                    cardsForOpponent = 4;
+                                    cardsForOpponent+=4;
                                     player1.getCards().remove(chosenCard);
                                     turn = player2;
                                 }
+                            }
+                            else if(chosenCard.getType()=="+2"){
+                                cardsForOpponent+=2;
+                                CardInCenter = chosenCard;
+                                player1.getCards().remove(chosenCard);
+                                turn = player2;
+                            }
+                            else if (chosenCard.getType()=="Block") {
+                                blockForOpponent++;
+                                CardInCenter = chosenCard;
+                                player1.getCards().remove(chosenCard);
+                                turn = player2;
+                            }
+                            else if(chosenCard.getType()=="Switch"){
+                                CardInCenter = chosenCard;
+                                player2.getCards().remove(chosenCard);
                             }
                             else {
                                 CardInCenter = chosenCard;
@@ -124,11 +202,34 @@ public class Main {
                 }
             }
             else if(turn==player2){
-                if(cardsForOpponent!=0){
+                System.out.println("The card in the center is: " + CardInCenter);
+                if(blockForOpponent!=0){
+                    System.out.println("It's the turn of: " + player2.getName() + " you have been blocked for: " + blockForOpponent + " turns, if you dont have or dont want to use your block type in: 2137");
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Choose your card (type in 2137 if you dont have/dont want to use your + cards)");
+                    print_hand(player2.getCards());
+                    int whichCard = scan.nextInt();
+                    whichCard--;
+                    if(whichCard==2136){
+                        blockForOpponent--;
+                        turn=player1;
+                    }
+                    else {
+                        Card chosenCard = player2.getCards().get(whichCard);
+                        if (chosenCard.getType() == "Block") {
+                            CardInCenter = chosenCard;
+                            blockForOpponent++;
+                            turn = player1;
+                        } else if (player2.getCards().get(whichCard).getType() != "Block") {
+                            System.out.println("This is not a card you can use!");
+                        }
+                    }
+                }
+                else if(cardsForOpponent!=0){
                     System.out.println("It's the turn of: " + player2.getName() + " he has " + cardsForOpponent + " cards to draw, if he doesnt use a + card he will draw the amount");
                     Scanner scan = new Scanner(System.in);
                     System.out.println("Choose your card (type in 2137 if you dont have/dont want to use your + cards)");
-                    System.out.println(player2.getCards());
+                    print_hand(player2.getCards());
                     int whichCard = scan.nextInt();
                     whichCard--;
                     if(whichCard==2136){
@@ -142,32 +243,38 @@ public class Main {
                     }
                     else{
                         Card chosenCard = player2.getCards().get(whichCard);
-                        if(chosenCard.getType()=="+2"){
-                            cardsForOpponent+=2;
-                            turn = player1;
-                        }
-                        else{
-                            Scanner color = new Scanner(System.in);
-                            System.out.println("Choose the color (Red,Green,Blue,Yellow");
-                            String CardColor = color.nextLine();
-                            CardInCenter = new Card("0",CardColor);
-                            cardsForOpponent+=4;
-                            player2.getCards().remove(chosenCard);
-                            turn = player1;
+                            if(chosenCard.getType()=="+2"){
+                                CardInCenter = chosenCard;
+                                cardsForOpponent+=2;
+                                player2.getCards().remove(chosenCard);
+                                turn = player1;
+                            }
+                            else if(chosenCard.getType()=="+4"){
+                                Scanner color = new Scanner(System.in);
+                                System.out.println("Choose the color (Red,Green,Blue,Yellow");
+                                String CardColor = color.nextLine();
+                                CardInCenter = new Card("0",CardColor);
+                                cardsForOpponent+=4;
+                                player2.getCards().remove(chosenCard);
+                                turn = player2;
+                            }
+                            else if(chosenCard.getType()!="2" || chosenCard.getType()!="+4"){
+                                System.out.println("You can't use this card to counter the + card!");
+                            }
                         }
                     }
-                }
-                else{
+                else if(cardsForOpponent==0 && blockForOpponent==0){
                     System.out.println("It's the turn of: " + player2.getName());
                     System.out.println("Your hand: ");
-                    System.out.println(player2.getCards());
+                    print_hand(player2.getCards());
                     Scanner scan = new Scanner(System.in);
                     System.out.println("Choose your card (type in 2137 to end turn)");
                     int whichCard = scan.nextInt();
                     whichCard--;
                     if(whichCard==2136){
-                        player2.getCards().add(Deck.get(0));
-                        Deck.remove(Deck.get(0));
+                        int cardId2 = (int)(Math.random() * Deck.size());
+                        player2.getCards().add(Deck.get(cardId2));
+                        Deck.remove(Deck.get(cardId2));
                         turn = player1;
                     }
                     else{
@@ -191,6 +298,23 @@ public class Main {
                                     player2.getCards().remove(chosenCard);
                                     turn = player1;
                                 }
+
+                            }
+                            else if(chosenCard.getType()=="+2"){
+                                cardsForOpponent+=2;
+                                CardInCenter = chosenCard;
+                                player2.getCards().remove(chosenCard);
+                                turn = player1;
+                            }
+                            else if (chosenCard.getType()=="Block") {
+                                blockForOpponent++;
+                                CardInCenter = chosenCard;
+                                player2.getCards().remove(chosenCard);
+                                turn = player1;
+                            }
+                            else if(chosenCard.getType()=="Switch"){
+                                CardInCenter = chosenCard;
+                                player2.getCards().remove(chosenCard);
                             }
                             else {
                                 CardInCenter = chosenCard;
@@ -203,7 +327,6 @@ public class Main {
                         }
                     }
                 }
-
             }
         }
     }
